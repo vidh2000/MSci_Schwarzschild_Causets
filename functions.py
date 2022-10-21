6381fffcb03e98ce6516b01258a183b746775183
vh119 <vid.homsak19@imperial.ac.uk>
2022-10-21 10:14:40 +0100

    Added TO DO list + testing on causal diamonds

--- functions.py
+++ functions.py
@@ -2,25 +2,30 @@ import numpy as np
 import cProfile
 import pstats
 
-def profiler(function,args=None):
+def profiler(function,Nshow=None,args=None):
     """
     Profiles the function "function" with arguments
     "args" and returns the output of function(args)
     if args =! None.
     Processed are sorted by the TIME spent inside each of them
-    and printed out.
+    and printed out. Shows "Nshow" operations
     """
     if args == None:
         with cProfile.Profile() as pr:    
             output = function()
             stats = pstats.Stats(pr)
             stats.sort_stats(pstats.SortKey.TIME)
-            stats.print_stats()
-        
+            if Nshow == None:
+                stats.print_stats()
+            else:
+                stats.print_stats(Nshow)    
     else:
         with cProfile.Profile() as pr:    
             output = function(args)
             stats = pstats.Stats(pr)
             stats.sort_stats(pstats.SortKey.TIME)
-            stats.print_stats()
+            if Nshow == None:
+                stats.print_stats()
+            else:
+                stats.print_stats(Nshow)
     return output
