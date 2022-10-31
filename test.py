e6f36c5fdf6fee23ccdf59836733d27deb095187
vh119 <vid.homsak19@imperial.ac.uk>
2022-10-31 22:17:40 +0000

    Added cmatrix+sets method in causet_new.cpp

--- test.py
+++ test.py
@@ -1,29 +1,11 @@
-#%%
-from math import nan
 import numpy as np
-import random as rand
 
 
-text=input()
+dim=4
+N=int(10**4)
+coords = np.random.rand(N,dim)
 
-def letter_counter(word):
-    
-    # Store all letters in a list
-    letters = []
-    print("List of letters = ", letters)
-    for letter in word:
-        letters.append(letter)
-    # Find all unique letters in the word
-    characters_set = sorted(set(letters))
-    # Count number of times the unique character appears in the word
-    dictionary = dict()
-    for character in characters_set:
-        count=0
-        for letter in letters:
-            if character == letter:
-                count +=1
-        dictionary[character] = count
-    return dictionary
 
-result = letter_counter(text)
-print(result)
+
+
+
