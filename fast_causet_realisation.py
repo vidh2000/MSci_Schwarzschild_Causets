009d2e1dff1f32bf81ad1ebd99021a811910080d
vh119 <vid.homsak19@imperial.ac.uk>
2022-11-01 01:06:09 +0000

    Semi-implemented parallel processing. Not working

--- fast_causet_realisation.py
+++ fast_causet_realisation.py
@@ -1,9 +1,11 @@
 #%%
-from pickletools import int4
+
 import numpy as np
 from time import time
 from tqdm import tqdm
 import multiprocessing as mp
+from functools import partial
+from numba import *
 
 def make_coords(N,dim,min=0,max=1,sorted=True):
     coords = []
@@ -23,7 +25,38 @@ def areTimelike(x,y,dim):
     else:
         return False
 
-
+@jit
+def make_causet_matrix(coords):
+    dims, dim, size = (len(coords[0]),len(coords[0])), len(coords[0]),len(coords)
+    cm = np.zeros(dims,dtype=int)
+    for i in range(size):
+        for j in range(i):
+            if cm[i][j] ==1:
+                continue
+            else:
+                dt2 = (coords[i][0]-coords[j][0])*(coords[i][0]-coords[j][0])
+                dspace2 = sum([(coords[i][p]-coords[j][p])*
+                        (coords[i][p]-coords[j][p]) for p in range(1,dim)])
+                ds2 = dt2-dspace2 
+                if ds2>0:
+                    cm[i][j] = 1
+                    ## Inherit the past
+                    #cm[i,:] = np.bitwise_or(cm[i,:], cm[j,:])
+                else:
+                    pass
+            
+    return cm
+        
+def mp_add_pasts(pasts, coords, dim):
+    for i in range(len(pasts)):
+        for j in reversed(range(i)):
+            if j in pasts[i]:
+                continue
+            else:
+                if areTimelike(coords[i],coords[j],dim):
+                    pasts[i].add(j)
+                    pasts[i].update(pasts[j])
+    return pasts
 
 class Causet():
     def __init__(self, coordinates, mode = "cmatrix"):
@@ -40,49 +73,66 @@ class Causet():
             raise ValueError("Please provide 'cmatrix' or 'pasts' as method\
                                 parameter.")
     
-    def make_past_sets(self):
-        
-        for i in tqdm(range(self.size)):
-            for j in reversed(range(i)):
-                if j in self.pasts[i]:
-                    continue
-                else:
-                    if areTimelike(self.coords[i],self.coords[j],self.dim):
-                        self.pasts[i].add(j)
-                        self.pasts[i].update(self.pasts[j])
+
+    def make_past_sets(self,multiprocessing=True):
+        if multiprocessing:
+            with mp.Pool() as pool:
+                self.pasts = pool.starmap(mp_add_pasts,
+                            zip(self.pasts, self.coords,
+                                [self.dim for i in range(self.size)]))
+            pool.close()
+            pool.join()
+        else:
+            for i in tqdm(range(self.size)):
+                for j in reversed(range(i)):
+                    if j in self.pasts[i]:
+                        continue
+                    else:
+                        if areTimelike(self.coords[i],self.coords[j],self.dim):
+                            self.pasts[i].add(j)
+                            self.pasts[i].update(self.pasts[j])
                 
-    def make_cmatrix(self):
-        
-        dims = (self.size,self.size)
-        cm = np.zeros(dims,dtype=int)
-        for i in tqdm(range(self.size)):
-            for j in reversed(range(i)):
-                if cm[i][j] ==1:
-                    continue
-                else:
-                    if areTimelike(self.coords[i],coords[j],self.dim):
-                        cm[i][j] = 1
-                        # Inherit the past
-                        cm[i,:] = np.bitwise_or(cm[i,:], cm[j,:])
-        self.causal_matrix = cm
+    def make_cmatrix(self,jit=False):
+        # Uses jit 
 
-dim=2
-N=int(10**4)
-
-coords = make_coords(N,dim)
-
-
-# 'pasts'
-start =  time()
-Causet(coords,mode = "pasts")
-duration = round(time()-start,3)
-print(f"Time taken for 'pasts', t = {duration}s")
+        if jit:
+            self.causal_matrix = make_causet_matrix(self.coords)
+        else:
+            dims = (self.size,self.size)
+            cm = np.zeros(dims,dtype=np.int32)
+            for i in range(self.size):
+                for j in range(i):
+                    if cm[i][j] == 1:
+                        continue
+                    else:
+                        if areTimelike(self.coords[i],self.coords[j],self.dim):
+                            cm[i][j] = 1
+                            # Inherit the past
+                            cm[i,:] = np.bitwise_or(cm[i,:], cm[j,:])
+                        else:
+                            pass
 
 
-# 'cmatrix'
-start =  time()
-Causet(coords,mode = "cmatrix")
-duration = round(time()-start,3)
-print(f"Time taken for 'cmatrix', t = {duration}s")
+if __name__=="__main__":
+    dim=4
+    N=int(10**3)
+
+    coords = make_coords(N,dim)
+
+    # 'pasts'
+    start =  time()
+    c = Causet(coords,mode = "pasts")
+    duration = round(time()-start,3)
+    print(f"Time taken for 'pasts' for N={N} in D={dim}\nt = {duration}s")
+
+    print(c.pasts[100])
+
+    ## 'cmatrix'
+    #start =  time()
+    #Causet(coords,mode = "cmatrix")
+    #duration = round(time()-start,3)
+    #print(f"Time taken for 'cmatrix', t = {duration}s")
 
 
+
+# %%
