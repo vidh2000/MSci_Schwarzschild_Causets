500804ef2dfa1b8095d4ea7821caa728a5635631
vh119 <vid.homsak19@imperial.ac.uk>
2022-11-13 22:20:23 +0100

    pybind11 works. Builds modules into build/Release

--- cpp_to_py/cpp_to_py.cpp
+++ cpp_to_py/cpp_to_py.cpp
@@ -1,5 +1,18 @@
+#include <pybind11/pybind11.h>
 #include <iostream>
-#include <pybind11\pybind11.h>
+
+/*
+Produced module is in build/Release and can be imported as
+import module_name inside a python file (with relevant).
+
+To do it.
+Go to build folder.
+In the command line, write:
+
+cmake ..
+cmake --build . --target ALL_BUILD --config Release
+
+*/
 
 namespace py = pybind11;
 
@@ -7,7 +20,7 @@ int add(int i, int j) {
     return i + j;
 }
 
-PYBIND11_MODULE(example, m) {
+PYBIND11_MODULE(module_name, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
     m.def("add", &add, "A function that adds two numbers");
