e5f8514805d7f5c7c1bbb3e7e45d2c0eed6cc822
vh119 <vid.homsak19@imperial.ac.uk>
2022-11-13 17:51:09 +0100

    Pybind update

--- cpp_to_py/cpp_to_py.cpp
+++ cpp_to_py/cpp_to_py.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <pybind11\pybind11.h>
 
-int main(){
-  
-  std::cout << "Hello world!\n";
+namespace py = pybind11;
+
+int add(int i, int j) {
+    return i + j;
 }
 
+PYBIND11_MODULE(example, m) {
+    m.doc() = "pybind11 example plugin"; // optional module docstring
+
+    m.def("add", &add, "A function that adds two numbers");
+}
