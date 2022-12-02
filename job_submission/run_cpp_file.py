8c002239b38e61752a7e96bb4cfadb01c4ce5a67
vh119 <vid.homsak19@imperial.ac.uk>
2022-12-02 15:43:33 +0000

    submission job updated files

--- job_submission/run_cpp_file.py
+++ job_submission/run_cpp_file.py
@@ -4,33 +4,35 @@ cwd = os.getcwd()
 
 # Path to MSci_Schwarzschild_Causets
 file_dir = os.path.dirname(os.path.realpath(__file__))
-main_dir = os.path.dirname(file_dir)
-print("Main dir:\n", main_dir)
+main_dir = os.path.dirname(file_dir)+"\\"
 
 # Directory path of causets_cpp
-causets_cpp = main_dir + "/scripts_cpp/causets_cpp"
+causets_cpp = main_dir + "scripts_cpp\\causets_cpp\\"
+
+print(causets_cpp)
 
 # Specify which file you want to run
-runfile_name = main_dir #+ ..........
-
+runfileDir = f"{main_dir}scripts_cpp\\tests\\"
+runfilename = "test_hello_world.cpp"
 # Compile commands
 compiler_stuff = [
-    f"g++ -g {runfile_name}",
-    f"{causets_cpp}/causet.cpp",
-    f"{causets_cpp}/embeddedcauset.cpp",
-    f"{causets_cpp}/sprinkledcauset.cpp",
-    f"{causets_cpp}/shapes.cpp",
-    f"{causets_cpp}/spacetimes.cpp",
+    f"g++ -g '{runfileDir+runfilename}'",
+    f"'{causets_cpp}causet.cpp'",
+    f"'{causets_cpp}embeddedcauset.cpp'",
+    f"'{causets_cpp}sprinkledcauset.cpp'",
+    f"'{causets_cpp}shapes.cpp'",
+    f"'{causets_cpp}spacetimes.cpp'",
 ]
 
 # Create Executable
-exec = [
+exec = [ " ",
     "-o",
-    f"{runfile_name[:-4]}"
+    #"'output.exe'"
+    f"'{runfileDir}{runfilename[:-4]}.exe'"
 ]
 
 #Optimisation flags
-optimisations = [
+optimisations = [ " ",
     "-Ofast",
     #"-march=native",
     #"-ffast-math",
@@ -38,18 +40,30 @@ optimisations = [
     ]
 
 # Include paths
-includes = [
+includes = [" ",
     "-I",
-    "/rds/general/user/vh119/home/boost/boost_1_80_0",
+    "'C:\\Program Files\\boost\\boost_1_80_0'",
+    #"/rds/general/user/vh119/home/boost/boost_1_80_0",
     ]
 
 # Compile
 
 to_render = " ".join(compiler_stuff) + " ".join(exec) + \
             " ".join(optimisations) + " ".join(includes)
+
+print("To render:")
+#print(to_render)
 os.system(to_render)
 
 # Run the compiled executable
+
+print("Go into folder:")
+#print(runfileDir)
+os.system("d:")
+os.system(f"cd '{runfileDir}'")
+
+print("To execute:")
+#print(f"./{runfilename[:-4]}")
 os.system(f"./{exec[1]}")
 
 
---
+++ scripts_cpp/tests/test_hello_world.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <numeric>
+#include <set>
+#include <stack>
+#include <stdio.h>
+
+
+
+int main(){
+    std::cout << "Hello World!" << std::endl;
+}
