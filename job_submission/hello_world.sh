f0b57b6efc2fbf22b1037f2e3fedb13f32cb3d03
vidh2000 <vid.homsak19@imperial.ac.uk>
2023-01-09 14:06:47 +0000

    cmd line parsing to cpp and err/out cp

---
+++ job_submission/hello_world.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo First line, testing cluster submittion
+echo 
+echo Hello world
+
+echo Finished testing, should output this text, hopefully no errors
---
+++ job_submission/submitter.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# State which file you want to submit
+
+
+echo "#!/bin/sh" >> submitme.pbs
