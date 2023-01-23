928afc723019da5127a75c4fa7fa10f4202dc6fd
vidh2000 <vid.homsak19@imperial.ac.uk>
2023-01-23 19:53:31 +0000

    int8_t works!

--- job_submission/run_file.sh
+++ job_submission/run_file.sh
@@ -23,24 +23,22 @@ runfileRelativeDir="scripts_cpp/for_submitting_cpp/"
 ########################################################################
 
 # Get useful directories
-fileDir=$(pwd)"/"
-mainDir="$(dirname "$fileDir")/"
+homeDir="${HOME}/MSci_Schwarzschild_Causets/"
+job_submissionsDir="${homeDir}job_submission/"
+submitted_jobsDir="${job_submissionsDir}submitted_jobs/"
 
-
-
-#echo $fileDir
-#echo $mainDir
+mainDir=$homeDir
 
 # Run the python file which will compile the .cpp file you want
 # (the includes,flags... can be adjusted in run_cpp_file.py)
 echo Building the executable...
-python run_cpp_file.py $mainDir$runfileRelativeDir $runfilename
+python ${job_submissionsDir}run_cpp_file.py $mainDir$runfileRelativeDir $runfilename
 echo Finished building the executable
 
 # Go into the directory of the executable file
 cd $mainDir$runfileRelativeDir
 
 
-# Execute the created .exe program 
-#echo ${runfilename::-4}
-./${runfilename::-4}".exe" 
+# Execute the copy of the created .exe program
+cp ${runfilename::-4}".exe" "executables/${runfilename::-4}.exe"
+./executables/${runfilename::-4}".exe" 
