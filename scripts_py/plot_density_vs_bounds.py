031d50f7b09dcee6e7363c6bf90740c96eff82a1
vidh2000 <vid.homsak19@imperial.ac.uk>
2023-01-24 16:46:54 +0000

    New data and fixing lambdas plotting

--- scripts_py/plot_density_vs_bounds.py
+++ scripts_py/plot_density_vs_bounds.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 from os.path import expanduser
 from causets_py import causet_helpers as ch
-
+import pandas as pd
 
 ##############################################################################
 # 0. SET USER VARIABLES
@@ -34,17 +34,10 @@ else:
     plotsDir = path + f"/figures/density_vs_bounds/"
     dataDir = path + f"/data/test_boundary_vs_density/"
 
-# mass_string must have 6 characters, being M=I.ab
+# mass_string must have 6 characters
 mass = round(mass, 2)
-mass_string = f"M={mass}"
-if len(mass_string) > 6:
-    mass_string = mass_string[:7]
-elif len(mass_string) < 6:
-    while len(mass_string) < 6:
-        if "." not in mass_string:
-            mass_string += ".00"
-        else:
-            mass_string.append("0")
+mass_string = f"M={round(mass,2)}"
+
 
 
 
@@ -72,106 +65,83 @@ molecules_distr_std = []
 for root, dirs, files in os.walk(dataDir):
     # for each file file_i
     for i, file_i in enumerate(files):
-        if mass_string in file_i:
-            go_on = False
-            if want_rho and "Rho" in file_i:
-                go_on = True
-                file_path = os.path.join(root, file_i)
-                file_pieces = file_i.split("_")
-                # get rho of file_i
-                for piece_j in file_pieces:
-                    if piece_j[0:3] == "Rho":
-                        rho_i = piece_j.split("=")[1]
-                        if rho_i not in rhos:
-                            rhos.append(rho_i)
-                        break
-            elif want_Nmult and "Nmult" in file_i:
-                go_on = True
-                file_path = os.path.join(root, file_i)
-                file_pieces = file_i.split("_")
-                # get nmult of file_i
-                for piece_j in file_pieces:
-                    if piece_j[0:3] == "Nmult":
-                        nmult_i = piece_j.split("=")[1]
-                        if nmult_i not in nmults:
-                            nmults.append(nmult_i)
-                        break
-            # if the file is correct
-            if go_on: 
-                # 2.1 get Nreps ############################################
-                Nreps_i = np.loadtxt(file_i, delimeter = ",", skiprows=1,
-                                            usecols = 0)
-                # 2.2 get outermost, innermost, mintime info ################
-                outermosts_avg_arr_i = np.loadtxt(file_i, delimeter = ",", 
-                                            skiprows=1,
-                                            usecols = 1)
-                outermosts_std_arr_i = np.loadtxt(file_i, delimeter = ",", 
-                                            skiprows=1,
-                                            usecols = 2)
-                innermosts_avg_arr_i = np.loadtxt(file_i, delimeter = ",", 
-                                            skiprows=1,
-                                            usecols = 3)
-                innermosts_std_arr_i = np.loadtxt(file_i, delimeter = ",", 
-                                            skiprows=1,
-                                            usecols = 4)
-                mintimes_avg_arr_i = np.loadtxt(file_i, delimeter = ",", 
-                                            skiprows=1,
-                                            usecols = 5)
-                mintimes_std_arr_i = np.loadtxt(file_i, delimeter = ",", 
-                                            skiprows=1,
-                                            usecols = 6)
-                outermost_i, outermost_std_i = ch.std_combine(Nreps_i,
-                                                        outermosts_avg_arr_i,
-                                                        outermosts_std_arr_i,)
-                innermost_i, innermost_std_i = ch.std_combine(Nreps_i,
-                                                        outermosts_avg_arr_i,
-                                                        outermosts_std_arr_i,)
-                mintime_i, mintime_std_i = ch.std_combine(Nreps_i,
-                                                        outermosts_avg_arr_i,
-                                                        outermosts_std_arr_i,)
-                outermosts    .append(outermost_i)
-                outermosts_std.append(outermost_std_i)
-                innermosts    .append(innermost_i)
-                innermosts_std.append(innermost_std_i)
-                mintimes      .append(mintime_i)
-                mintimes_std  .append(mintime_std_i)
-                # 2.3 get molecules info #####################################
-                mol_info_i = np.loadtxt(file_i, delimeter = ",", 
-                                        skiprows=1) [:,7:]
-                ntypes = mol_info_i.shape[1]/2
-                if (ntypes - int(ntypes)): 
-                    raise Exception(f"Number of cols {ntypes*2} is odd")
-                molecules_distr.append([])
-                molecules_distr_std.append([])
-                for n in range(ntypes):
-                    avgs_mol_n_i = mol_info_i[:,2*n]
-                    stds_mol_n_i = mol_info_i[:,2*n+1]
-                    n_mol_n_i, std_mol_n_i = ch.std_combine(Nreps,
-                                                            avgs_mol_n_i,
-                                                            stds_mol_n_i)
-                    # if the nth molecule had already been found
-                    try:
-                        molecules_distr[n]    .append(n_mol_n_i)
-                        molecules_distr_std[n].append(std_mol_n_i)
-                    #if the nth molecule was never found
-                    except IndexError: 
-                        # make zeros for previous (general for rho and nmult)
-                        n_prev_rhos = max(len(rhos), len(nmults)) -1
-                        zeros = np.zeros(n_prev_rhos).tolist()
-                        molecules_distr    .append(zeros + [n_mol_n_i]) 
-                        molecules_distr_std.append(zeros + [std_mol_n_i])
+        
+        if f"M={round(mass,2)}" not in file_i:
+            continue
 
-# Clear a bit
-del root, dirs, files
-del file_path, file_pieces, piece_j
-del i, file_i
-del outermosts_avg_arr_i, outermosts_std_arr_i, outermost_i, outermost_std_i
-del innermosts_avg_arr_i, innermosts_std_arr_i, innermost_i, innermost_std_i
-del mintimes_avg_arr_i, mintimes_std_arr_i, mintime_i, mintime_std_i 
-del mol_info_i, avgs_mol_n_i, stds_mol_n_i, n_mol_n_i, std_mol_n_i, 
-del n, ntypes, zeros
+        # Store the density for each file in array    
+        rhos.append(float(file_i.split("_M=")[0][4:]))
+
+        file_i = root + file_i
+
+        # 2.1 get Nreps ############################################
+        Nreps_i = pd.read_csv(file_i, delimiter = ",",
+                                skiprows=1).iloc[:, 0].tolist()
+
+        print(Nreps_i)
+        # 2.2 get outermost, innermost, mintime info ################
+        outermosts_avg_arr_i = np.loadtxt(file_i, delimiter = ",", 
+                                    skiprows=1,
+                                    usecols = 1)
+        outermosts_std_arr_i = np.loadtxt(file_i, delimiter = ",", 
+                                    skiprows=1,
+                                    usecols = 2)
+        innermosts_avg_arr_i = np.loadtxt(file_i, delimiter = ",", 
+                                    skiprows=1,
+                                    usecols = 3)
+        innermosts_std_arr_i = np.loadtxt(file_i, delimiter = ",", 
+                                    skiprows=1,
+                                    usecols = 4)
+        mintimes_avg_arr_i = np.loadtxt(file_i, delimiter = ",", 
+                                    skiprows=1,
+                                    usecols = 5)
+        mintimes_std_arr_i = np.loadtxt(file_i, delimiter = ",", 
+                                    skiprows=1,
+                                    usecols = 6)
+        outermost_i, outermost_std_i = ch.std_combine(Nreps_i,
+                                                outermosts_avg_arr_i,
+                                                outermosts_std_arr_i,)
+        innermost_i, innermost_std_i = ch.std_combine(Nreps_i,
+                                                outermosts_avg_arr_i,
+                                                outermosts_std_arr_i,)
+        mintime_i, mintime_std_i = ch.std_combine(Nreps_i,
+                                                outermosts_avg_arr_i,
+                                                outermosts_std_arr_i,)
+        outermosts    .append(outermost_i)
+        outermosts_std.append(outermost_std_i)
+        innermosts    .append(innermost_i)
+        innermosts_std.append(innermost_std_i)
+        mintimes      .append(mintime_i)
+        mintimes_std  .append(mintime_std_i)
+
+        # 2.3 get molecules info #####################################
+        mol_info_i = np.loadtxt(file_i, delimiter = ",", 
+                                skiprows=1) [:,7:]
+        ntypes = mol_info_i.shape[1]/2
+        if (ntypes - int(ntypes)): 
+            raise Exception(f"Number of cols {ntypes*2} is odd")
+        molecules_distr.append([])
+        molecules_distr_std.append([])
+        for n in range(ntypes):
+            avgs_mol_n_i = mol_info_i[:,2*n]
+            stds_mol_n_i = mol_info_i[:,2*n+1]
+            n_mol_n_i, std_mol_n_i = ch.std_combine(Nreps,
+                                                    avgs_mol_n_i,
+                                                    stds_mol_n_i)
+            # if the nth molecule had already been found
+            try:
+                molecules_distr[n]    .append(n_mol_n_i)
+                molecules_distr_std[n].append(std_mol_n_i)
+            #if the nth molecule was never found
+            except IndexError: 
+                # make zeros for previous (general for rho and nmult)
+                n_prev_rhos = max(len(rhos), len(nmults)) -1
+                zeros = np.zeros(n_prev_rhos).tolist()
+                molecules_distr    .append(zeros + [n_mol_n_i]) 
+                molecules_distr_std.append(zeros + [std_mol_n_i])
 
 
+print(rhos)
 ##############################################################################
 # 2. PLOT
 ##############################################################################
@@ -235,6 +205,7 @@ if plot_boundaries:
 
 
 # 2.1 MOLECULES'S DISTRIBUTION ##############################################
+"""
 if plot_molecules:
     fig, ax = plt.figure(f"Molecules for {mass_string}")
     for n in range(len(molecules_distr)):
@@ -253,7 +224,7 @@ if plot_molecules:
     plt.xlabel('Horizon Area [\ell^2]')
     plt.ylabel("Occurrences")
     plt.grid(alpha = 0.2) 
-
+"""
 
 
 
