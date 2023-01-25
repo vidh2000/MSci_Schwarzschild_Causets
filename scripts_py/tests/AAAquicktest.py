7f19f46106595a02a9bae8c8b4bcf7686c9ddbf3
StefanoV00 <stefanoveroni00@gmail.com>
2023-01-25 16:13:37 +0000

    quicktest

--- scripts_py/tests/AAAquicktest.py
+++ scripts_py/tests/AAAquicktest.py
@@ -1,5 +1,31 @@
 import numpy as np
 
-string = "abcdexyz"
+mass = 1
+Rho = np.array([400, 500, 600, 700, 800, 900, 1000])
+scale = Rho **(-1/4)
+R = 2*mass+3*scale
+r = 2*mass-3*scale
+T = 4*scale
+h = r/R
+N = Rho * (4*3.1415/3) * (R*R*R-r*r*r) * T
+print("\n",N)
 
-print(string[-4:-1])
+mass = 2
+Rho = np.array([400, 500, 600, 700, 800, 900, 1000])
+scale = Rho **(-1/4)
+R = 2*mass+3*scale
+r = 2*mass-3*scale
+T = 4*scale
+h = r/R
+N = Rho * (4*3.1415/3) * (R*R*R-r*r*r) * T
+print("\n",N)
+
+mass = 3
+Rho = np.array([400, 500, 600, 700, 800, 900, 1000, 1200])
+scale = Rho **(-1/4)
+R = 2*mass+3*scale
+r = 2*mass-3*scale
+T = 4*scale
+h = r/R
+N = Rho * (4*3.1415/3) * (R*R*R-r*r*r) * T
+print("\n",N)
