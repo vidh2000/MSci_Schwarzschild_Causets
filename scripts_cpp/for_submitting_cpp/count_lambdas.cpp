18b10a2fa43a4d7ec2141e6721ea5b2b2bf3c999
vidh2000 <vid.homsak19@imperial.ac.uk>
2023-01-25 11:33:06 +0000

    added hollow to count_lambdas

--- scripts_cpp/for_submitting_cpp/count_lambdas.cpp
+++ scripts_cpp/for_submitting_cpp/count_lambdas.cpp
@@ -208,6 +208,10 @@ for (auto && tup : boost::combine(cards, radii, hollow_vals,
     std::stringstream stream3;
     stream3 << std::fixed << std::setprecision(2) << durations[0];
     std::string dur_str = stream3.str();
+    // Saving hollow used when set as global param in testing
+    std::stringstream stream4;
+    stream4 << std::fixed << std::setprecision(2) << hollow;
+    std::string hollow_str = stream4.str();
 
     std::string filename = std::string(homeDir) 
                             + "/MSci_Schwarzschild_Causets/data/lambdas/"
@@ -215,6 +219,7 @@ for (auto && tup : boost::combine(cards, radii, hollow_vals,
                             + "_Nmult=" + rho_str //tho currently Nmult
                             + "_Card=" + std::to_string(cards[0])
                             + "_r=" + radius_str
+                            + "_hollow=" + hollow_str
                             + "_dur=" + dur_str
                             + ".txt";
     std::cout<<"\n==========================================================\n";
