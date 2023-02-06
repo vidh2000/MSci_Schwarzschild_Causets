48605425e31a8483dfb38c1636d3623eb0d411c3
vidh2000 <vid.homsak19@imperial.ac.uk>
2023-02-06 15:03:05 +0000

    Job resource with const plot

--- job_submission/job_resources_estimater.py
+++ job_submission/job_resources_estimater.py
@@ -1,16 +1,25 @@
 import numpy as np
 
 
-masses = [1, 1.5, 2, 2.5, 2.6, 3]
+# Areas in l^2 units
+#areas_ll = 4*np.pi*(2*mass)**2/(scale**2)
+
+areas_ll = np.arange(1000,20001,1000)
 rhos = np.array([5000])
 int8_t = False
 
 for Rho in rhos:
     print("")
+    
+    # Set scale (discreteness length)
+    scale = Rho **(-1/4)
+    print(f"Rho={Rho:.0f}, Scale = {scale:.3f}")
+   
+    masses = np.sqrt(areas_ll/(16*np.pi*np.sqrt(Rho)))
+    print("Masses:")
+    print(masses, "\n")
+
     for mass in masses:
-        
-        # Set scale (discreteness length)
-        scale = Rho **(-1/4)
 
         # Set sprinkling region
         R = 2*mass+3*scale
@@ -26,7 +35,8 @@ for Rho in rhos:
         else:
             mem = N**2 * 4 / 1073741824 #in GB
 
-        # Print require information
-        print(f"Rho={Rho}, M={mass:.2f}, N={N:.0f} \
-        RAM={mem:.0f}GB") #add time later
 
+        
+        # Print require information
+        print(f"Rho={Rho}, M={mass:.2f}, Area={4*np.pi*(2*mass)**2/(scale**2):.0f} l^2, N={N:.0f} \
+        RAM={mem:.0f}GB") #add time later
