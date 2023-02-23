6a1f1d5f1414a617a480aaa00b9afba0cde05dfd
vidh2000 <vid.homsak19@imperial.ac.uk>
2023-02-23 00:34:42 +0000

    Failed jobs..

--- job_submission/HRV_counting_submitter.sh
+++ job_submission/HRV_counting_submitter.sh
@@ -14,13 +14,13 @@ cpp_file_to_run="'count_HRVs.cpp'"  # need 'filename.cpp' inside the string!
 
 # CPP VARIABLES
 Rho=5000
-N_reps=10
+N_reps=30
 
 
 # CLUSTER JOB RESOURCE REQUIREMENTS
 ncpus=256
 mem=920
-runtime="04:00:00" #format: "hh:mm:ss"
+runtime="08:00:00" #format: "hh:mm:ss"
 
 
 # SET MASSES YOU WANT TO SIMULATE
