9f122563e9ffb31258c0aa58cc15bf28e7d5a2f2
vidh2000 <vid.homsak19@imperial.ac.uk>
2023-06-04 21:11:05 +0100

    Subd jobs up to 3.0929. finishd 2.7-3.0005 row

---
+++ job_submission/submitted_jobs/runfile_files/run_file_lambdacounting_mass_2.78_Rho5000_N_reps_40_Nth5.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+
+# .cpp file you want to run in
+
+runfilename='count_lambdas_3D.cpp'
+
+### Specify relative path from MSci_Schwarzschild_Causets
+# to the .cpp file you want to compile/execute
+runfileRelativeDir='scripts_cpp/for_submitting_cpp/'
+
+# Get useful directories
+mainDir='/rds/general/user/vh119/home/MSci_Schwarzschild_Causets/'
+
+# Run the python file which will compile the .cpp file you want
+# (the includes,flags... can be adjusted in run_cpp_file.py)
+echo Building the executable...
+python /rds/general/user/vh119/home/MSci_Schwarzschild_Causets/job_submission/run_cpp_file.py $mainDir$runfileRelativeDir $runfilename
+echo Finished building the executable
+
+echo Go into the directory of the executable file:
+cd $mainDir$runfileRelativeDir
+
+
+mass=2.78
+Rho=5000
+N_reps=40
+Nth=5
+
+# Execute the copy of the created .exe program
+cp ${runfilename::-4}".exe" "executables/lambdas_M${mass}_Rho${Rho}_reps${N_reps}_Nth${Nth}.exe"
+"./executables/lambdas_M${mass}_Rho${Rho}_reps${N_reps}_Nth${Nth}.exe" $mass $Rho $N_reps
+
