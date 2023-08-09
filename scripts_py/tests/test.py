92fce44a474ded283b05231d72a0b7f2c05f503a
Vid Homsak <vid.homsak19@imperial.ac.uk>
2023-08-09 23:55:58 +0200

    checking git

--- scripts_py/tests/test.py
+++ scripts_py/tests/test.py
@@ -1,30 +1,11 @@
-import pandas as pd
-from os.path import expanduser
-import os
+import numpy as np
 
 
-# Home Directory
-home = expanduser("~")
-# Path
-path = os.getcwd()
-# specify the input file path
-folder = home + f"/MSci_Schwarzschild_Causets/data/test_boundary_vs_density/"
+a = np.zeros(10)
 
-# iterate over the files in the folder
-for file in os.listdir(folder):
-    if "new" in file:
-        continue
-    # read the CSV file into a DataFrame
-    df = pd.read_csv(os.path.join(folder, file),
-                header=None)
-    # transpose the data
-    df = df.transpose()
-    # drop rows that contains NaN data
-    df = df.dropna()
-    # write the transposed data to the original file
-    nn = file+"_new"
-    data = df.to_string(index=False, header=False, justify='left',
-            max_colwidth=20)
-    # write the data to the original file
-    with open(os.path.join(folder, file), 'w') as f:
-        f.write(data)
+b = np.array([1,1,1,1,1,1,1,1])
+
+v = b*b
+
+print(v)
+
